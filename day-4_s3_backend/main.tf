756ada885d92babeb188bfa511e9ca03add3d6a0
rajesh <kurumojurajeshkumar535@gmail.com>
2024-05-11 13:43:36 +0530

    f

--- day-4_s3_backend/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "raj" {
-    bucket = "remote-s3-local"
-  
-}
--- day-4_s3_backend/s3-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "remote-s3-local"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
-
