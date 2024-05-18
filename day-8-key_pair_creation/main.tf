93f2521adb6995fb6081a3daf4ceece61f1df520
rajesh <kurumojurajeshkumar535@gmail.com>
2024-05-18 22:00:11 +0530

    day-8 updates

---
+++ day-8-key_pair_creation/main.tf
@@ -0,0 +1,15 @@
+#create public key_pair in local
+resource "aws_key_pair" "raj" {
+  key_name   = "public"
+ public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK1otx/NcMDGCHI+cTvki+NM7HfUlWmQKArUBkZTaw3 kurum@MSI"
+}
+
+#create ec2 instance
+resource "aws_instance" "raj" {
+  ami = "ami-013e83f579886baeb"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.raj.key_name
+  tags = {
+    Name = "raj"
+ }
+}
