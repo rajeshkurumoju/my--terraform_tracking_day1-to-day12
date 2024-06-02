bcdb9edb369fde3654ee5ecc5f6b81393c75f593
rajeshkurumoju <168176879+rajeshkurumoju@users.noreply.github.com>
2024-06-02 21:58:55 +0530

    Update main.tf

--- day-2/main.tf
+++ day-2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "raj" {
     Name = "raj"
   }
 }
-#resource "aws_s3_bucket" "name" {
-  #bucket = "duihiuhdsdwf"
+resource "aws_s3_bucket" "name" {
+  bucket = "duihiuhdsdwf"
   
-#}
+}
